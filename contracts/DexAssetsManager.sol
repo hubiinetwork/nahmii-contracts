41d077f95028d9121d868346f5b74781a1560556
Hernán Di Pietro <indiocolifa@hotmail.com>
2018-01-29 18:31:41 -0300

    Begin Omphalos 0.6 specification compliance

--- contracts/DexAssetsManager.sol
+++ contracts/DexAssetsManager.sol
@@ -14,33 +14,32 @@ import './SafeMath.sol';
 
 contract DexAssetsManager {
 
-	/* The time-window for a withdraw request to be released, in minutes */
 	uint256 constant private WITHDRAW_RELEASE_TIME_WIN_MINUTES = 1; 
 
 	address private owner;
 
-	struct TransactionInfo {
-		uint256 amount;
-		uint256 timestamp;
-		address token;      //0 for ethers
-	}
-
 	struct PerUserInfo {
 		uint256 tradeNonce;
-		TransactionInfo[] deposits;
 
-		// Current account balance of ethers and tokens.
-		uint256 etherBalance;
-		mapping (address => uint256) tokenBalance;
+		// Active balance of ethers and tokens.
+		uint256 activeEtherBalance;
+		mapping (address => uint256) activeTokenBalance;
 
-		// Current maximum allowed withdrawal amount for ethers and tokens.
-		uint256 allowedEthersWithdrawal;
-		mapping (address => uint256) allowedTokensWithdrawal;
-
-		TransactionInfo[] withdrawal;
+		// Staged balance of ethers and tokens.
+		uint256 stagedEtherBalance;
+		mapping (address => uint256) stagedTokenBalance;
 	}
 
 	mapping (address => PerUserInfo) private userInfoMap;
+
+	struct Trade {}
+
+	struct Settlement {
+		// uint256[] tradeNonces;
+		// uint256[] tradeHashes;
+		// // Balances here
+		// uint256[] signature;
+	}
 
 	//events
 	event OwnerChanged(address oldOwner, address newOwner);
@@ -60,14 +59,14 @@ contract DexAssetsManager {
 		OwnerChanged(oldOwner, newOwner);
 	}
 
+	// 
+	// Deposit functions
+	// -------------------------------------------------------------------------------------------------------------------------
+
 	function () public payable {
 		require(msg.sender != owner);
 		require(msg.value > 0);
-
-		//add deposit transaction
-		userInfoMap[msg.sender].deposits.push(TransactionInfo(msg.value, now, 0));
-
-		//raise event
+		userInfoMap[msg.sender].activeEtherBalance = SafeMath.add(userInfoMap[msg.sender].activeEtherBalance, msg.value);
 		Deposit(msg.sender, msg.value, 0);
 	}
 
@@ -78,111 +77,88 @@ contract DexAssetsManager {
 		ERC20 token = ERC20(tokenAddress);
 		require(token.balanceOf(msg.sender) >= amount);
 
-		// Add deposit transaction and increment balance.
-		userInfoMap[msg.sender].deposits.push(TransactionInfo(amount, now, tokenAddress));
-		userInfoMap[msg.sender].tokenBalance[tokenAddress] = SafeMath.add(userInfoMap[msg.sender].tokenBalance[tokenAddress], amount);
-
-		// Raise event
+		userInfoMap[msg.sender].activeTokenBalance[tokenAddress] = SafeMath.add(userInfoMap[msg.sender].activeTokenBalance[tokenAddress], amount);
 		Deposit(msg.sender, amount, tokenAddress);
 	}
 
-	function getDepositsCount(address user) public view onlyOwner returns (uint256) {
-		return userInfoMap[user].deposits.length;
-	}
+	//
+	// Balance functions
+	// -------------------------------------------------------------------------------------------------------------------------
 
-	function getDeposits(address user, uint index) public view onlyOwner returns (uint256 amount, uint256 timestamp, address token) {
-		// must check address?
-		require (index < userInfoMap[user].deposits.length);
-		amount = userInfoMap[user].deposits[index].amount;
-		timestamp = userInfoMap[user].deposits[index].timestamp;
-		token = userInfoMap[user].deposits[index].token;
-	}
-
-	function getEtherBalance(address wallet) public view onlyOwner returns (uint256) {
+	function activeBalance(address wallet, address token) public view onlyOwner returns (uint256) {
 		require(wallet != address(0));
-		return userInfoMap[wallet].etherBalance;
+		return token == address(0x0) ? userInfoMap[wallet].activeEtherBalance : userInfoMap[wallet].activeTokenBalance[token];
 	}
 
-	function getTokenBalance(address wallet, address token) public view onlyOwner returns (uint256) {
+	function stagedBalance(address wallet, address token) public view onlyOwner returns (uint256) {
 		require(wallet != address(0));
-		require(token != address(0));
-		return userInfoMap[wallet].tokenBalance[token];
+		return token == address(0x0) ? userInfoMap[wallet].stagedEtherBalance : userInfoMap[wallet].stagedTokenBalance[token];
 	}
 
-	function approveEthersWithdrawal(address beneficiary, uint256 amount) public onlyOwner {
-		require(beneficiary != address(0));
+	function unstage(uint256 amount, address tokenAddress) {
 		require(amount > 0);
+		require(msg.sender != owner);
+	}
+	
+	//
+	// Trade Challenge/Settlement Functions
+	// -------------------------------------------------------------------------------------------------------------------------
+	function startTradeOrdersChallenge(uint256[] startNonces, uint256[] endNonces, uint256 tradeCount) {
 
-		userInfoMap[beneficiary].allowedEthersWithdrawal = SafeMath.add(userInfoMap[beneficiary].allowedEthersWithdrawal, amount);
 	}
 
-	function approveTokensWithdrawal(address tokenAddress, address beneficiary, uint256 amount) public onlyOwner {
-		require(tokenAddress != address(0));
-		require(beneficiary != address(0));
-		require(amount > 0);
+	function startLastTradeChallenge() {
 
-		userInfoMap[beneficiary].allowedTokensWithdrawal[tokenAddress] = SafeMath.add(userInfoMap[beneficiary].allowedTokensWithdrawal[tokenAddress], amount);
 	}
 
-	function withdrawEthers() public {
-		uint256 amount = userInfoMap[msg.sender].allowedEthersWithdrawal;
-		userInfoMap[msg.sender].allowedEthersWithdrawal = 0;
+	function challengeTradeOrder(Trade t) {
 
-		if (amount > 0) {
-			//add withdrawal transaction
-			userInfoMap[msg.sender].withdrawal.push(TransactionInfo(amount, now, 0));
+	}
 
-			//transfer money to sender's wallet
+	function challengeLastTrade(Trade t) {
+
+	}
+
+	function settleTrade(Trade t, address wallet ) {
+
+	}
+
+	function settleTrades(Settlement s, Trade t, address wallet ) {
+
+	}
+
+	function tradeOrdersChallengeStage(Trade t) returns (uint256) {
+		return 0;
+	}
+
+	function lastTradeChallengeStage(Trade t) returns (uint256) {
+		return 0;
+	}
+
+	function VoteOnTradeOrders(Trade t, uint256 option) {
+
+	}
+
+	//
+	// Withdraw functions
+	// -------------------------------------------------------------------------------------------------------------------------
+
+	function withdraw(uint256 amount, address tokenAddress) public {
+		require (msg.sender != owner);
+		require (amount > 0);
+		if (token == address(0x0)) {
+			require (amount <= userInfoMap[msg.sender].activeEtherBalance);
 			msg.sender.transfer(amount);
-		}
-
-		//raise event
-		Withdraw(msg.sender, amount, 0);
-	}
-
-
-	function withdrawTokens(address tokenAddress) public {
-		require(tokenAddress != address(0));
-		ERC20 token = ERC20(tokenAddress);
-
-		uint256 amount = userInfoMap[msg.sender].allowedTokensWithdrawal[tokenAddress];
-		userInfoMap[msg.sender].allowedTokensWithdrawal[tokenAddress] = 0;
-
-		if (amount > 0) {
-			//add withdrawal transaction
-			userInfoMap[msg.sender].withdrawal.push(TransactionInfo(amount, now, tokenAddress));
-
-			//transfer tokens to sender's
+			userInfoMap[msg.sender].activeEtherBalance = SafeMath.sub(userInfoMap[msg.sender].activeEtherBalance, amount);
+		} else {
+			require (amount <= userInfoMap[msg.sender].activeTokenBalance[msg.sender]);
+			ERC20 token = ERC20(tokenAddress);
 			token.transfer(msg.sender, amount);
+			userInfoMap[msg.sender].activeTokenBalance[token] = SafeMath.sub(userInfoMap[msg.sender].activeTokenBalance[token], amount);
 		}
 
 		//raise event
-		Withdraw(msg.sender, amount, tokenAddress);
-	}
-
-	/// @notice Execute a given trade, adjusting the token/ether balance, for subsequent withdrawal requests.
-	/// @param tradeNonce 		A trade-engine provided nonce for this trade. Should be consecutive for this wallet requests.
-	/// @param wallet     		The involved wallet address on this trade.
-	/// @param token			The token address for the token contract involved in this trade.
-	/// @param netAmountEth  	The total net amount of Ethers involved in this trade.
-	/// @param netAmountToken   The total net amount of Tokens involved in this trade.
-	function executeTrade(uint256 tradeNonce, address wallet, address token, uint256 netAmountEth, uint256 netAmountToken) public onlyOwner {
-		require(netAmountEth != 0);
-		require(netAmountToken != 0);
-		require(wallet != address(0));
-		require(token != address(0));
-		require(tradeNonce == userInfoMap[wallet].tradeNonce + 1);
-
-		uint256 netEthResult = SafeMath.add(userInfoMap[wallet].allowedEthersWithdrawal, netAmountEth); 
-		uint256 netTokenResult = SafeMath.add(userInfoMap[wallet].allowedTokensWithdrawal[token], netAmountToken);
-		require(netEthResult >= 0);
-		require(netTokenResult >= 0);
-
-		userInfoMap[wallet].allowedEthersWithdrawal = netEthResult;
-		userInfoMap[wallet].allowedTokensWithdrawal[token] = netTokenResult;
-		userInfoMap[wallet].tradeNonce = tradeNonce;
-		
-		TradeCommitted(tradeNonce, wallet);
+		Withdraw(msg.sender, amount, token);
 	}
 
 	modifier onlyOwner() {
