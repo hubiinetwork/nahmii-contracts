176e70fe2aac7bc660f445c3b76597670ea791a7
hernandp <hernan.di.pietro@gmail.com>
2018-04-06 14:09:28 -0300

    ReserveFunds R001-R016 test suite

---
+++ test/DexReserveFunds.js
@@ -0,0 +1,286 @@
+/*!
+ * Hubii Network - DEX Reserve Funds Smart Contract test suite.
+ *
+ * Copyright (C) 2017-2018 Hubii
+ */
+
+var DexReserveFunds = artifacts.require("DexReserveFunds");
+var ERC20Token = artifacts.require("StandardTokenEx");
+var keccak256 = require("augmented-keccak256");
+
+contract('DexReserveFunds', function () {
+    async = require('async');
+    ethers = require('ethers');
+    w3prov = new ethers.providers.Web3Provider(web3.currentProvider);
+    const address_zero = '0x0000000000000000000000000000000000000000';
+    const coinbase = web3.eth.coinbase;
+    const user_a = web3.eth.accounts[1];
+    const user_b = web3.eth.accounts[2];
+    const user_c = web3.eth.accounts[3];
+    const user_d = web3.eth.accounts[4];
+    const signer_a = w3prov.getSigner(user_a);
+    const signer_b = w3prov.getSigner(user_b);
+    const signer_c = w3prov.getSigner(user_c);
+    const signer_d = w3prov.getSigner(user_d);
+    const signer_owner = w3prov.getSigner(coinbase);
+    const gasLimit = 1800000;
+    const ctraddr = DexReserveFunds.address;
+    const kTokenSupply = 1000;
+    const kTokensForUser1 = 50;
+    var deployedDex = null;
+    var deployedDexIo = null; // Uses Ethers.io for convenience using structs,etc.
+    var deployedErc20 = null;
+    var ltc_signature = null;
+
+    //-------------------------------------------------------------------------
+    // Contract-wide helper functions
+    //-------------------------------------------------------------------------
+
+    function _verifyDeposit(user, index, token, amount, opts, cb) {
+        deployedDex.deposit(user, index, opts).then(function (args) {
+            const _amount = args[0];
+            const _timestamp = args[1];
+            const _token = args[2];
+
+            if (token == 0 && (web3.toWei(amount, 'ether') != _amount)) {
+                cb(new Error("Unexpected ether deposit amount"));
+                return;
+            }
+            if (token != 0 && amount != _amount) {
+                cb(new Error("Unexpeced token deposit amount"));
+                return;
+            }
+            if (_timestamp == 0) {
+                cb(new Error("Timestamp cannot be null"));
+                return;
+            }
+            cb(null);
+        }, function (err) {
+            cb(err);
+        });
+    }
+
+    function signTrade(t, signer) {
+        var hash = keccak256.create();
+        hash.update(t);
+        var tradeHashWithoutSignature = hash.digest();
+        var signedMessage = web3.eth.sign(signer, '0x' + tradeHashWithoutSignature);
+
+        var result = {};
+        result.r = "0x" + signedMessage.substr(2, 64);
+        result.s = "0x" + signedMessage.substr(66, 64);
+        result.v = parseInt(signedMessage.substr(130, 2), 16) + 27;
+        return result;
+    }
+
+    //-------------------------------------------------------------------------
+    // Preflight stage
+    //-------------------------------------------------------------------------
+
+    before("Preflight: Check available account addresses and balances", function (done) {
+        assert.notEqual(user_a, null);
+        assert.notEqual(user_b, null);
+        assert.notEqual(user_c, null);
+        assert.notEqual(user_d, null);
+
+        assert.ok(web3.fromWei(web3.eth.getBalance(user_a), "ether") > 10);
+        assert.ok(web3.fromWei(web3.eth.getBalance(user_b), "ether") > 10);
+        assert.ok(web3.fromWei(web3.eth.getBalance(user_c), "ether") > 10);
+        assert.ok(web3.fromWei(web3.eth.getBalance(user_d), "ether") > 10);
+
+        done();
+    });
+
+    before("Preflight: get deployed contracts", function (done) {
+        DexReserveFunds.deployed().then(function (_d) {
+            assert.notEqual(_d, null);
+            deployedDex = _d;
+            deployedDexIo = new ethers.Contract(_d.address, DexReserveFunds.abi, w3prov);
+            deployedErc20 = ERC20Token.new().then(function (_t) {
+                assert.notEqual(_t, null);
+                deployedErc20 = _t;
+                _t.totalSupply = kTokenSupply;
+                done();
+            },
+                function () {
+                    done(new Error('Failed to create ERC20Token instance'));
+                });
+        },
+            function () {
+                done(new Error('Failed to get deployed contract address'));
+            });
+    });
+
+    before("Preflight: distribute test tokens", function (done) {
+        deployedErc20.testMint(user_a, kTokensForUser1).then(function () {
+            done();
+        }, function (err) {
+            done(new Error('Cannot assign kTokensForUser1: ' + err.toString()));
+        });
+    });
+
+
+    //-------------------------------------------------------------------------
+    // Tests start here
+    //
+    //
+    // Test description format:
+    // TXX: MUST {SUCCEED/FAIL} [function_name]: result of successful execution, or reason for fail.
+    //
+    // Returned error strings in Done() callback function:
+    // 'This test must fail'
+    // 'This test must succeed'
+    // Additional info can be provided after the messages above. e.g: err.toString()
+    //
+    //-------------------------------------------------------------------------
+
+    it("R001: MUST FAIL [payable]: cannot be called from owner", function (done) {
+        web3.eth.sendTransaction({ from: coinbase, to: ctraddr, value: web3.toWei(10, 'ether'), gas: gasLimit }, function (err) {
+            done(err == null ? new Error('This test must fail') : null);
+        });
+    });
+
+    it("R002: MUST FAIL [payable]: cannot be called with 0 ethers", function (done) {
+        web3.eth.sendTransaction({ from: user_a, to: ctraddr, value: web3.toWei(0, 'ether'), gas: gasLimit }, function (err) {
+            done(err == null ? new Error('This test must fail') : null);
+        });
+    });
+
+    it("R003: MUST SUCCEED [payable]: add 2.5 Ethers to user A aggregate balance", function (done) {
+        web3.eth.sendTransaction({ from: user_a, to: ctraddr, value: web3.toWei(2.5, 'ether'), gas: gasLimit }, function (err) {
+            done(err != null ? new Error('This test must succeed. Error: ' + err.toString()) : null);
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R004: MUST SUCCEED [payable]: add 6.5 Ethers to user B aggregate balance", function (done) {
+        web3.eth.sendTransaction({ from: user_b, to: ctraddr, value: web3.toWei(6.5, 'ether'), gas: gasLimit }, function (err) {
+            done(err != null ? new Error('This test must succeed. Error: ' + err.toString()) : null);
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R005: MUST SUCCEED [depositTokens]: 5 tokens added to A aggregate balance", function (done) {
+        deployedErc20.approve(deployedDex.address, 5, { from: user_a }).then(function (args) {
+            deployedDex.depositTokens(deployedErc20.address, 5, { from: user_a }).then(function (args) {
+                done();
+            }, function (err) {
+                done(new Error('This test must succeed. Error: ' + err.toString()));
+            });
+        }, function () {
+            done(new Error('This test must succeed. Error: ERC20 failed to approve token transfer'));
+        })
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R006: MUST FAIL [depositTokens]: Cannot be called from owner address", function (done) {
+        deployedDex.depositTokens(deployedErc20.address, 5, { from: coinbase }).then(function (args) {
+            done(new Error('This test must fail'));
+        }, function (err) {
+            done();
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R007: MUST FAIL [depositTokens]: Cannot be called with zero address", function (done) {
+        deployedDex.depositTokens(0, 5, { from: user_a }).then(function (args) {
+            done(new Error('This test must fail'));
+        }, function (err) {
+            done();
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R008: MUST FAIL [depositTokens]: Cannot be called with zero amount", function (done) {
+        deployedDex.depositTokens(deployedErc20.address, 0, { from: user_a }).then(function (args) {
+            done(new Error('This test must fail'));
+        }, function (err) {
+            done();
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R009: MUST FAIL [depositTokens]: User does not have enough tokens to deposit.", function (done) {
+        deployedErc20.approve(deployedDex.address, 9999, { from: user_a }).then(function (args) {
+            deployedDex.depositTokens(deployedErc20.address, 9999, { from: user_a }).then(function (args) {
+                done(new Error('This test must fail'));
+            }, function (err) {
+                done();
+            });
+        }, function () {
+            done(new Error('This test must fail. Error: ERC20 failed to approve token transfer'));
+        })
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R010: MUST SUCCEED [depositCount]: User A should have 2 deposits", function (done) {
+        deployedDex.depositCount(user_a).then(function (count) {
+            done(count != 2 ? new Error('This test must succeed. Error: Deposit count: ' + count) : null);
+        }, function (err) {
+            done(new Error('This test must succeed. Error: ' + err.toString()));
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R011: MUST SUCCEED [depositCount]: User B should have 1 deposit", function (done) {
+        deployedDex.depositCount(user_b).then(function (count) {
+            done(count != 1 ? new Error('This test must succeed. Error: Deposit count: ' + count) : null);
+        }, function (err) {
+            done(new Error('This test must succeed. Error: ' + err.toString()));
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R012: MUST FAIL [depositCount]: Cannot be called from non-owner address", function (done) {
+        deployedDex.depositCount(user_a, { from: user_a }).then(function (count) {
+            done(new Error('This test must fail'));;
+        }, function (err) {
+            done();
+        });
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R013: MUST SUCCEED [deposit]: User B should have 6.5 ETH at index 0", function (done) {
+        _verifyDeposit(user_b, 0, 0, 6.5, {}, done);
+    });
+
+    //-------------------------------------------------------------------------
+
+    it("R014: MUST FAIL [deposit]: Invalid index deposit 1 for user B.", function (done) {
+        _verifyDeposit(user_b, 1, 0, 6.5, {}, function (err) {
+            done(err == null ? new Error('This test must fail') : null);
+        });
+    });
+
+    //------------------------------------------------------------------------
+
+    it("R015: MUST SUCCEED [deposit]: User A should have 5 tokens at index 1", function (done) {
+        _verifyDeposit(user_a, 1, deployedErc20.address, 5, {}, done);
+    });
+
+    //------------------------------------------------------------------------
+
+    it("R016: MUST FAIL [deposit]: Cannot be called from non-owner address", function (done) {
+        _verifyDeposit(user_a, 1, deployedErc20.address, 5, { from: user_a }, function (err) {
+            done(err == null ? new Error('This test must fail') : null);
+        });
+    });
+
+    //------------------------------------------------------------------------
+
+});
+
+
+
+
+
