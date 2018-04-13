40da114db8fc186ef3749b128b7007a36080184b
Jens Ivar Jordre <jensivar@hubii.com>
2018-04-13 16:00:14 +0200

    Addition of smart contract skeletons

---
+++ contracts/CommunityVote.sol
@@ -0,0 +1,57 @@
+/*!
+ * Hubii Network - DEX Smart Contract for assets settlement.
+ *
+ * Compliant to Omphalos 0.11 Specification.
+ *
+ * Copyright (C) 2017-2018 Hubii
+ */
+pragma solidity ^0.4.21;
+
+contract CommunityVote {
+
+    //
+    // Variables
+    // -----------------------------------------------------------------------------------------------------------------
+    address private owner;
+
+    //
+    // Events
+    // -----------------------------------------------------------------------------------------------------------------
+    event OwnerChangedEvent(address oldOwner, address newOwner);
+
+    //
+    // Constructor
+    // -----------------------------------------------------------------------------------------------------------------
+    function CommunityVote(address _owner) public notNullAddress(_owner) {
+        owner = _owner;
+    }
+
+    //
+    // Functions
+    // -----------------------------------------------------------------------------------------------------------------
+    function changeOwner(address newOwner) public onlyOwner notNullAddress(newOwner) {
+        address oldOwner;
+
+        if (newOwner != owner) {
+            // Set new owner
+            oldOwner = owner;
+            owner = newOwner;
+
+            // Emit event
+            emit OwnerChangedEvent(oldOwner, newOwner);
+        }
+    }
+
+    //
+    // Modifiers
+    // -----------------------------------------------------------------------------------------------------------------
+    modifier notNullAddress(address _address) {
+        require(_address != address(0));
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
