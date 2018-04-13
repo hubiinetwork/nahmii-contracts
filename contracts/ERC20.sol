27174f180b034321e968be5c9c6e0275442ba27f
Jens Ivar Jordre <jensivar@hubii.com>
2018-04-13 18:17:58 +0200

    Add ERC20 and SafeMath

---
+++ contracts/ERC20.sol
@@ -0,0 +1,17 @@
+/*!
+* Hubii - Omphalos
+*
+* Copyright (C) 2017-2018 Hubii AS
+*/
+pragma solidity ^0.4.21;
+
+contract ERC20 {
+    uint256 public totalSupply;
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
