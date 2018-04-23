72585ebe970fb4bf8044a3a71406b9296a811d34
Jens Ivar Jordre <jensivar@hubii.com>
2018-04-23 11:03:53 +0200

    Add code from abandoned Trade smart contract

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,14 +10,43 @@ pragma solidity ^0.4.21;
 /**
 @title Exchange
 @notice The orchestrator of trades and payments on-chain.
-@dev The essence and trade specific parts of the previous Trade smart contract.
 */
 contract Exchange {
 
     //
+    // Structures
+    // -----------------------------------------------------------------------------------------------------------------
+    struct Signature {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+    }
+
+    struct Trade {
+        uint256 buyOrderHash;
+        uint256 sellOrderHash;
+        uint256 buyerOrderNonce;    // These ones should be obtained from orders map (accessed by hash),
+        uint256 sellerOrderNonce;   // but we don't have such information.
+        address buyer;
+        address seller;
+        uint256 tokenAmount;
+        uint256 etherAmount;
+        address token;
+        bool immediateSettlement;
+        Signature signature;
+    }
+
+    struct Settlement {
+        // uint256[] tradeNonces;
+        // uint256[] tradeHashes;
+        // // Balances here
+        // uint256[] signature;
+    }
+
+    //
     // Variables
     // -----------------------------------------------------------------------------------------------------------------
-    address private owner;
+    address public owner;
 
     //
     // Events
