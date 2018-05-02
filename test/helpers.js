cf25bbf4167ec8bac055bb0cecc3bdbb518e7414
Mauro Leggieri <mxmauro@mauroleggieri.com>
2018-05-02 14:31:19 -0300

    Augmented Web3 now uses BlueBird.js

--- test/helpers.js
+++ test/helpers.js
@@ -1,61 +1,12 @@
+var BlueBird = require("bluebird");
+
 module.exports.augmentWeb3 = function (web3)
 {
-	web3.eth.sendTransactionPromise = function(transactionObject) {
-		return new Promise((resolve, reject) => {
-			web3.eth.sendTransaction(transactionObject, function (err) {
-				if (!err)
-					resolve();
-				else
-					reject(err);
-			});
-		});
-	}
-
-	web3.eth.getBalancePromise = function(addressHexString) {
-		return new Promise((resolve, reject) => {
-			web3.eth.getBalance(addressHexString, function (err, balance) {
-				if (!err)
-					resolve(balance);
-				else
-					reject(err);
-			});
-		});
-	}
-
-	web3.eth.getTransactionPromise = function (transactionHash) { 
-		return new Promise(function (resolve, reject) {
-			web3.eth.getTransaction(transactionHash, function (err, receipt) {
-				if (!err)
-					resolve(receipt);
-				else
-					reject(err);
-			});
-		});
-	}
-
-	web3.eth.getTransactionReceiptPromise = function (hashString) { 
-		return new Promise(function (resolve, reject) {
-			web3.eth.getTransactionReceipt(hashString, function (err, receipt) {
-				if (!err)
-					resolve(receipt);
-				else
-					reject(err);
-			});
-		});
-	}
-
-	web3.eth.getBlockPromise = function (blockHashOrBlockNumber, returnTransactionObjects) { 
-		return new Promise(function (resolve, reject) {
-			if (typeof returnTransactionObjects === 'undefined')
-				returnTransactionObjects = false;
-			web3.eth.getBlock(transactionHash, returnTransactionObjects, function (err, receipt) {
-				if (!err)
-					resolve(receipt);
-				else
-					reject(err);
-			});
-		});
-	}
+	web3.eth.sendTransactionPromise = BlueBird.promisify(web3.eth.sendTransaction);
+	web3.eth.getBalancePromise = BlueBird.promisify(web3.eth.getBalance);
+	web3.eth.getTransactionPromise = BlueBird.promisify(web3.eth.getTransaction);
+	web3.eth.getTransactionReceiptPromise = BlueBird.promisify(web3.eth.getTransactionReceipt);
+	web3.eth.getBlockPromise = BlueBird.promisify(web3.eth.getBlock);
 }
 
 module.exports.TestCounter = function ()
