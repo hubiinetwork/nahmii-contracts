98711762cd05e6215c2b61c0880ed7d4ebf4ba4a
Jens Ivar Jordre <jensivar@hubii.com>
2018-05-13 01:27:23 +0200

    Update data availability response and add unit tests

--- test/scenarios/CommunityVote.js
+++ test/scenarios/CommunityVote.js
@@ -1,5 +1,35 @@
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+const utils = require('ethers').utils;
+
+chai.use(chaiAsPromised);
+chai.should();
+
 module.exports = function (glob) {
-	describe("CommunityVote", function () {
-		//NOTE: Currently no tests for this SC because it is almost a NO-OP
-	});
+    describe('CommunityVote', () => {
+        let ethersInstance;
+
+        before(() => {
+            ethersInstance = glob.ethersIoCommunityVote;
+        });
+
+        describe('getDoubleSpenders()', () => {
+            it('should return 0', async () => {
+                const result = await ethersInstance.getDoubleSpenders();
+                result.should.equal(utils.bigNumberify(0));
+            });
+        });
+        describe('getHighestAbsoluteDealNonce()', () => {
+            it('should return 0', async () => {
+                const result = await ethersInstance.getHighestAbsoluteDealNonce();
+                result.should.equal(utils.bigNumberify(0));
+            });
+        });
+        describe('isDataAvailable()', () => {
+            it('should return true', async () => {
+                const result = await ethersInstance.isDataAvailable();
+                result.should.be.true;
+            });
+        });
+    });
 };
