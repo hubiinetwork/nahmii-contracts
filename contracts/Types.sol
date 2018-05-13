0e33833c44571f13d2f27634de54b4a6f98f4dba
Jens Ivar Jordre <jensivar@hubii.com>
2018-05-13 19:26:19 +0200

    Factor out types and hash functions from Exchange and Fraud contracts

---
+++ contracts/Types.sol
@@ -0,0 +1,162 @@
+pragma solidity ^0.4.23;
+
+/**
+ * @title     Types
+ * @dev       Data types
+ * Copyright  (C) 2017-2018 Hubii AS
+ */
+library Types {
+
+    //
+    // Enums
+    // -----------------------------------------------------------------------------------------------------------------
+    enum OperationalMode {Normal, Exit}
+    enum LiquidityRole {Maker, Taker}
+    enum CurrencyRole {Intended, Conjugate}
+    enum DealType {Trade, Payment}
+    enum Sidedness {OneSided, TwoSided}
+    enum TradePartyRole {Buyer, Seller}
+    enum PaymentPartyRole {Source, Destination}
+
+    //
+    // Structures
+    // -----------------------------------------------------------------------------------------------------------------
+    struct CurrentPreviousInt256 {
+        int256 current;
+        int256 previous;
+    }
+
+    struct SingleNetInt256 {
+        int256 single;
+        int256 net;
+    }
+
+    struct IntendedConjugateCurrentPreviousInt256 {
+        CurrentPreviousInt256 intended;
+        CurrentPreviousInt256 conjugate;
+    }
+
+    struct IntendedConjugateSingleNetInt256 {
+        SingleNetInt256 intended;
+        SingleNetInt256 conjugate;
+    }
+
+    struct IntendedConjugateAddress {
+        address intended;
+        address conjugate;
+    }
+
+    struct IntendedConjugateInt256 {
+        int256 intended;
+        int256 conjugate;
+    }
+
+    struct PartyExchangeHashes {
+        bytes32 party;
+        bytes32 exchange;
+    }
+
+    struct Order {
+        int256 amount;
+        PartyExchangeHashes hashes;
+        CurrentPreviousInt256 residuals;
+    }
+
+    struct Signature {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+    }
+
+    struct Seal {
+        bytes32 hash;
+        Signature signature;
+    }
+
+    struct PartyExchangeSeals {
+        Seal party;
+        Seal exchange;
+    }
+
+    struct TradeParty {
+        address _address;
+        uint256 nonce;
+        uint256 rollingVolume;
+        LiquidityRole liquidityRole;
+        Order order;
+        IntendedConjugateCurrentPreviousInt256 balances;
+        IntendedConjugateInt256 netFees;
+    }
+
+    struct Trade {
+        uint256 nonce;
+        bool immediateSettlement;
+        int256 amount;
+        int256 rate;
+
+        IntendedConjugateAddress currencies;
+
+        TradeParty buyer;
+        TradeParty seller;
+
+        // Positive intended transfer is always in direction from seller to buyer
+        // Positive conjugate transfer is always in direction from buyer to seller
+        IntendedConjugateSingleNetInt256 transfers;
+
+        IntendedConjugateInt256 singleFees;
+
+        Seal seal;
+        uint256 blockNumber;
+    }
+
+    struct PaymentParty {
+        address _address;
+        uint256 nonce;
+        CurrentPreviousInt256 balances;
+        int256 netFee;
+    }
+
+    struct Payment {
+        uint256 nonce;
+        bool immediateSettlement;
+        int256 amount;
+
+        address currency;
+
+        PaymentParty source;
+        PaymentParty destination;
+
+        // Positive transfer is always in direction from source to destination
+        SingleNetInt256 transfers;
+
+        int256 singleFee;
+
+        PartyExchangeSeals seals;
+        uint256 blockNumber;
+    }
+
+    struct Settlement {
+        uint256 nonce;
+        DealType dealType;
+        Sidedness sidedness;
+        address[2] wallets;
+    }
+
+    //
+    // Functions
+    // -----------------------------------------------------------------------------------------------------------------
+    // TODO Implement fully
+    function hashTrade(Types.Trade trade) internal pure returns (bytes32) {
+        return keccak256(bytes32(trade.nonce));
+    }
+
+    // TODO Implement fully
+    function hashPaymentAsParty(Types.Payment payment) internal pure returns (bytes32) {
+        return keccak256(bytes32(payment.nonce));
+    }
+
+    // TODO Implement fully
+    function hashPaymentAsExchange(Types.Payment payment) internal pure returns (bytes32) {
+        return keccak256(bytes32(payment.nonce));
+    }
+}
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -6,6 +6,7 @@
 
 var SafeMathIntLib = artifacts.require('./SafeMathInt.sol');
 var SafeMathUintLib = artifacts.require('./SafeMathUint.sol');
+var Types = artifacts.require('./Types.sol');
 var ClientFund = artifacts.require("./ClientFund.sol");
 var CommunityVote = artifacts.require("./CommunityVote.sol");
 var Configuration = artifacts.require("./Configuration.sol");
@@ -19,29 +20,32 @@ var TokenHolderRevenueFund = artifacts.require("./TokenHolderRevenueFund.sol");
 // -----------------------------------------------------------------------------------------------------------------
 
 module.exports = function (deployer, network, accounts) {
-	var ownerAccount = accounts[0];
+    var ownerAccount = accounts[0];
 
-	deployer.deploy(SafeMathIntLib);
-	deployer.link(SafeMathIntLib, ClientFund);
-	deployer.link(SafeMathIntLib, CommunityVote);
-	deployer.link(SafeMathIntLib, Configuration);
-	deployer.link(SafeMathIntLib, Exchange);
-	deployer.link(SafeMathIntLib, Fraud);
-	deployer.link(SafeMathIntLib, ReserveFund);
-	deployer.link(SafeMathIntLib, RevenueFund);
-	deployer.link(SafeMathIntLib, SecurityBond);
-	deployer.link(SafeMathIntLib, TokenHolderRevenueFund);
+    deployer.deploy(SafeMathIntLib);
+    deployer.link(SafeMathIntLib, ClientFund);
+    deployer.link(SafeMathIntLib, CommunityVote);
+    deployer.link(SafeMathIntLib, Configuration);
+    deployer.link(SafeMathIntLib, Exchange);
+    deployer.link(SafeMathIntLib, Fraud);
+    deployer.link(SafeMathIntLib, ReserveFund);
+    deployer.link(SafeMathIntLib, RevenueFund);
+    deployer.link(SafeMathIntLib, SecurityBond);
+    deployer.link(SafeMathIntLib, TokenHolderRevenueFund);
     deployer.deploy(SafeMathUintLib);
     deployer.link(SafeMathUintLib, Exchange);
     deployer.link(SafeMathUintLib, Fraud);
     deployer.link(SafeMathUintLib, RevenueFund);
-	deployer.deploy(ClientFund, ownerAccount);
-	deployer.deploy(CommunityVote, ownerAccount);
-	deployer.deploy(Configuration, ownerAccount);
-	deployer.deploy(Exchange, ownerAccount);
-	deployer.deploy(Fraud, ownerAccount);
-	deployer.deploy(ReserveFund, ownerAccount);
-	deployer.deploy(RevenueFund, ownerAccount);
-	deployer.deploy(SecurityBond, ownerAccount);
-	deployer.deploy(TokenHolderRevenueFund, ownerAccount);
+    deployer.deploy(Types);
+    deployer.link(Types, Exchange);
+    deployer.link(Types, Fraud);
+    deployer.deploy(ClientFund, ownerAccount);
+    deployer.deploy(CommunityVote, ownerAccount);
+    deployer.deploy(Configuration, ownerAccount);
+    deployer.deploy(Exchange, ownerAccount);
+    deployer.deploy(Fraud, ownerAccount);
+    deployer.deploy(ReserveFund, ownerAccount);
+    deployer.deploy(RevenueFund, ownerAccount);
+    deployer.deploy(SecurityBond, ownerAccount);
+    deployer.deploy(TokenHolderRevenueFund, ownerAccount);
 };
