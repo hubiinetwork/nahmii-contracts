506b9ad45890f379c9b0b15b9f7252259cf6e8d2
Mauro Leggieri <mxmauro@mauroleggieri.com>
2018-05-24 18:28:26 -0300

    Initial code for RevenueToken

--- contracts/RevenueToken.sol
+++ contracts/RevenueToken.sol
@@ -9,48 +9,210 @@ pragma solidity ^0.4.23;
 
 import './ERC20.sol';
 import './SafeMathUint.sol';
+import './Ownable.sol';
 
 /**
  * @title Standard token
  * @dev Basic implementation of the EIP20 standard token (also known as ERC20 token).
  */
-contract RevenueToken is ERC20 {
-    using SafeMathUint for uint;
+contract RevenueToken is ERC20, Ownable {
+    using SafeMathUint for uint256;
 
-    uint private total_supply;
-    address owner;
+    //
+    // Events
+    // -----------------------------------------------------------------------------------------------------------------
+    event Mint(address indexed to, uint256 amount);
+
+    //
+    // Variables
+    // -----------------------------------------------------------------------------------------------------------------
+    uint256 private totalSupply;
+
     address[] public holders;
-    mapping(address => uint) balances;
-    mapping(address => mapping(uint => uint)) balanceBlocks;
-    mapping(address => uint[]) balanceBlockNumbers;
-    mapping(address => mapping(address => uint)) private allowed;
+    mapping(address => bool) holdersMap;
+    uint private holderEnumIndex;
 
-    constructor() public {
-        owner = msg.sender;
-        balanceBlocks[msg.sender][block.number] = 0;
-        balanceBlockNumbers[msg.sender].push(block.number);
-        balances[msg.sender] = total_supply;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(uint256 => uint256)) balanceBlocks;
+    mapping(address => uint256[]) balanceBlockNumbers;
+    mapping(address => mapping(address => uint256)) private allowed;
+
+    //
+    // Constructor
+    // -----------------------------------------------------------------------------------------------------------------
+    constructor() public ERC20() Ownable(msg.sender) {
+        totalSupply = 0;
+
+        //balanceBlocks[msg.sender][block.number] = 0;
+        //balanceBlockNumbers[msg.sender].push(block.number);
+
+        holderEnumIndex = 0;
     }
 
+    //
+    // Functions
+    // -----------------------------------------------------------------------------------------------------------------
     /**
      * @dev transfer token for a specified address
      * @param to The address to transfer to.
      * @param value The amount to be transferred.
      */
-    function transfer(address to, uint value) public returns (bool success) {
+    function transfer(address _to, uint256 value) public returns (bool success) {
         balanceBlocks[msg.sender][block.number] = balances[msg.sender].mul(block.number.sub(balanceBlockNumbers[msg.sender].length > 0 ? balanceBlockNumbers[msg.sender][balanceBlockNumbers[msg.sender].length - 1] : 0));
         balanceBlockNumbers[msg.sender].push(block.number);
         balances[msg.sender] = balances[msg.sender].sub(value);
-        balanceBlocks[to][block.number] = balances[to].mul(block.number.sub(balanceBlockNumbers[to].length > 0 ? balanceBlockNumbers[to][balanceBlockNumbers[to].length - 1] : 0));
-        balanceBlockNumbers[to].push(block.number);
-        balances[to] = balances[to].sub(value);
-        holders.push(to);
-        emit Transfer(msg.sender, to, value);
+
+        balanceBlocks[_to][block.number] = balances[_to].mul(block.number.sub(balanceBlockNumbers[_to].length > 0 ? balanceBlockNumbers[_to][balanceBlockNumbers[_to].length - 1] : 0));
+        balanceBlockNumbers[_to].push(block.number);
+        balances[_to] = balances[_to].add(value);
+
+        //add _to the token holders list
+        if (!holdersMap[_to]) {
+            holdersMap[_to] = true;
+            holders.push(_to);
+        }
+
+        //raise event
+        emit Transfer(msg.sender, _to, value);
         return true;
     }
 
-    //IMPORTANT: access should be pubblic or only owner+token_holder_revenue_funds?
-    function balanceBlocksIn(address a, uint from, uint to) public constant returns (uint) {
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param account The address whose balance is to be queried.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address account) public view returns (uint256 balance) {
+        return balances[account];
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amout of tokens to be transfered
+     */
+    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+        uint256 allowance = allowed[from][msg.sender];
+
+        // Check is not needed because sub(allowance, value) will already throw if this condition is not met
+        // require(value <= allowance);
+        // SafeMath uses assert instead of require though, beware when using an analysis tool
+
+        balances[from] = balances[from].sub(value);
+        balances[to] = balances[to].add(value);
+        allowed[from][msg.sender] = allowance.sub(value);
+
+        //add to the token holders list
+        if (!holdersMap[to]) {
+            holdersMap[to] = true;
+            holders.push(to);
+        }
+
+        //raise event
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool success) {
+        // To change the approve amount you first have to reduce the addresses'
+        //  allowance to zero by calling `approve(spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        require(value == 0 || allowed[msg.sender][spender] == 0);
+
+        allowed[msg.sender][spender] = value;
+
+        //raise event
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Function to check the amount of tokens than an owner allowed to a spender.
+     * @param account address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifing the amount of tokens still avaible for the spender.
+     */
+    function allowance(address account, address spender) public view returns (uint256 remaining) {
+        return allowed[account][spender];
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     *
+     * approve should be called when allowed[_spender] == 0. To increment
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
+        
+        //raise event
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     *
+     * approve should be called when allowed[_spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseApproval(address _spender, uint256 _subtractedValue) public returns (bool success) {
+        uint256 oldValue = allowed[msg.sender][_spender];
+
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+
+        //raise event
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyOwner public returns (bool)
+    {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+
+        //add to the token holders list
+        if (!holdersMap[_to]) {
+            holdersMap[_to] = true;
+            holders.push(_to);
+        }
+
+        //raise events
+        emit Mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+
+
+
+    //IMPORTANT: access should be public or only owner+token_holder_revenue_funds?
+    function balanceBlocksIn(address a, uint256 from, uint256 to) public constant returns (uint256) {
         // Def:
         //  * balance block = balance * block span
         // 
@@ -103,15 +265,15 @@ contract RevenueToken is ERC20 {
             return 0;
         }
 
-        uint i = 0;
+        uint256 i = 0;
         while (i < balanceBlockNumbers[a].length
         && balanceBlockNumbers[a][i] <= from) {
             i++;
         }
 
-        uint low = 0 == i ? from : balanceBlockNumbers[a][i - 1];
+        uint256 low = 0 == i ? from : balanceBlockNumbers[a][i - 1];
 
-        uint res = balanceBlocks[a][i].mul(balanceBlockNumbers[a][i].sub(from)).div(balanceBlockNumbers[a][i].sub(low));
+        uint256 res = balanceBlocks[a][i].mul(balanceBlockNumbers[a][i].sub(from)).div(balanceBlockNumbers[a][i].sub(low));
         i++;
 
         while (i < balanceBlockNumbers[a].length
@@ -128,163 +290,25 @@ contract RevenueToken is ERC20 {
         return res;
     }
 
-    /*
-    MAURO-EDIT: I would replace activeHoldersAsBytes with a function that returns a fixed-size array
-                of a struct with holder address and a boolean if balance != 0. The function also
-                needs a starting index to begin scan.
+    function startHoldersEnum() onlyOwner public view {
+        holderEnumIndex = 0;
+    }
 
-                Although more calls required and we should check if blocknumber changed between two
-                calls, the goal is to avoid an out of memory (must check VM limitations) if active
-                holders with a non-zero balance becomes too large.
-    */
-    // ------------------------------------------------------------------------
-    // https://medium.com/@tmyjoe/dapps-how-to-get-elements-of-array-in-a-contract-c61b16b6c438
-    // ------------------------------------------------------------------------
-    function activeHoldersAsBytes() public constant returns (bytes activeHoldersBytes) {
-        uint counter = 0;
-        uint i;
-        uint k;
+    function holdersEnum() onlyOwner public view returns (address[]) {
+        address[] memory _holders = new address[](128);
+        uint256 counter = 0;
 
-        for (i = 0; i < holders.length; i++) {
-            if (balances[holders[i]] > 0) {
+        while (counter < 128 && holderEnumIndex < holders.length) {
+            if (balances[holders[holderEnumIndex]] > 0) {
+                _holders[counter] = holders[holderEnumIndex];
                 counter++;
             }
+            holderEnumIndex++;
         }
-
-        activeHoldersBytes = new bytes(20 * counter);
-        counter = 0;
-        for (i = 0; i < holders.length; i++) {
-            if (balances[holders[i]] > 0) {
-                bytes memory elem = toBytes(holders[i]);
-                for (k = 0; k < 20; k++) {
-                    activeHoldersBytes[counter++] = elem[k];
-                }
-            }
-        }
+        //while (counter < 128) {
+        //    _holders[counter] = address(0);
+        //    counter++;
+        //}
+        return  _holders;
     }
-
-    function toBytes(address a) private pure returns (bytes b){
-        assembly {
-            let m := mload(0x40)
-            mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
-            mstore(0x40, add(m, 52))
-            b := m
-        }
-    }
-
-    /**
-     * @dev Gets the balance of the specified address.
-     * @param account The address whose balance is to be queried.
-     * @return An uint representing the amount owned by the passed address.
-     */
-    function balanceOf(address account) public view returns (uint balance) {
-        return balances[account];
-    }
-
-    /**
-     * @dev Transfer tokens from one address to another
-     * @param from address The address which you want to send tokens from
-     * @param to address The address which you want to transfer to
-     * @param value uint the amout of tokens to be transfered
-     */
-    function transferFrom(address from, address to, uint value) public returns (bool success) {
-        uint allowance = allowed[from][msg.sender];
-
-        // Check is not needed because sub(allowance, value) will already throw if this condition is not met
-        // require(value <= allowance);
-        // SafeMath uses assert instead of require though, beware when using an analysis tool
-
-        balances[from] = balances[from].sub(value);
-        balances[to] = balances[to].add(value);
-        allowed[from][msg.sender] = allowance.sub(value);
-        emit Transfer(from, to, value);
-        return true;
-    }
-
-    /**
-     * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
-     * @param spender The address which will spend the funds.
-     * @param value The amount of tokens to be spent.
-     */
-    function approve(address spender, uint value) public returns (bool success) {
-
-        // To change the approve amount you first have to reduce the addresses'
-        //  allowance to zero by calling `approve(spender, 0)` if it is not
-        //  already 0 to mitigate the race condition described here:
-        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        require(value == 0 || allowed[msg.sender][spender] == 0);
-
-        allowed[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
-    }
-
-    /**
-     * @dev Function to check the amount of tokens than an owner allowed to a spender.
-     * @param account address The address which owns the funds.
-     * @param spender address The address which will spend the funds.
-     * @return A uint specifing the amount of tokens still avaible for the spender.
-     */
-    function allowance(address account, address spender) public view returns (uint remaining) {
-        return allowed[account][spender];
-    }
-
-    /**
-     * Atomic increment of approved spending
-     *
-     * Works around https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     */
-    function addApproval(address spender, uint addedValue) public returns (bool success) {
-        uint oldValue = allowed[msg.sender][spender];
-        allowed[msg.sender][spender] = oldValue.add(addedValue);
-        emit Approval(msg.sender, spender, allowed[msg.sender][spender]);
-        return true;
-    }
-
-    /**
-     * Atomic decrement of approved spending.
-     *
-     * Works around https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     */
-    function subApproval(address spender, uint subtractedValue) public returns (bool success) {
-
-        uint oldVal = allowed[msg.sender][spender];
-
-        if (subtractedValue > oldVal) {
-            allowed[msg.sender][spender] = 0;
-        } else {
-            allowed[msg.sender][spender] = oldVal.sub(subtractedValue);
-        }
-        emit Approval(msg.sender, spender, allowed[msg.sender][spender]);
-        return true;
-    }
-
-    /**
-     * @dev Provides an internal function for destroying tokens. Useful for upgrades.
-     */
-    function burnTokens(address account, uint value) internal {
-        balances[account] = balances[account].sub(value);
-        total_supply = total_supply.sub(value);
-        emit Transfer(account, 0, value);
-    }
-
-    /**
-     * @dev Provides an internal minting function.
-     */
-    function mintInternal(address receiver, uint amount) internal {
-        total_supply = total_supply.add(amount);
-        balances[receiver] = balances[receiver].add(amount);
-
-        // Beware: Address zero may be used for special transactions in a future fork.
-        // This will make the mint transaction appear in EtherScan.io
-        // We can remove this after there is a standardized minting event
-        emit Transfer(0, receiver, amount);
-    }
-
-    /* Testing purposes only */
-    function testMint(address receiver, uint amount) public {
-        mintInternal(receiver, amount);
-    }
-
 }
