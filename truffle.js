e9d280bcff1495f70a529fddafe877c928c814fa
Jens Ivar Jordre <jensivar@hubii.com>
2018-06-05 15:37:35 +0200

    Raise gas limit to 7m

--- truffle.js
+++ truffle.js
@@ -4,43 +4,43 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*",
-            gas: 6000000
+            gas: 7000000
         },
         ganache_for_test: {
             host: "localhost",
             port: 8456,
             network_id: "*",
-            gas: 6000000
+            gas: 7000000
         },
         testnet: {
             host: "52.208.46.161",
             port: 8549,
             network_id: "3",
-            gas: 6000000
+            gas: 7000000
         },
         mainnet: {
             host: "ethereum.hubii.com",
             port: 8545,
             network_id: "1",
-            gas: 6000000
+            gas: 7000000
         },
         develop: {
             host: "localhost",
             port: 9545,
             network_id: "*",
-            gas: 6000000
+            gas: 7000000
         },
         ganache: {
             host: "localhost",
             port: 7545,
             network_id: "*",
-            gas: 6000000
+            gas: 7000000
         },
         ganachecli: {
             host: "localhost",
             port: 8545,
             network_id: "*",
-            gas: 6000000
+            gas: 7000000
         }
     },
     solc: {
