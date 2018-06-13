619a64935409561f9d1a0c181d58df0f6d93d04c
Jens Ivar Jordre <jensivar@hubii.com>
2018-06-13 11:05:40 +0200

    Add initial incomplete Validator unit test

--- test/main.js
+++ test/main.js
@@ -154,6 +154,17 @@ contract('Smart contract checks', function () {
         }
     });
 
+    before("Preflight: Instantiate Validator contract", async () => {
+        try {
+            glob.web3Validator = await Validator.deployed();
+            assert.notEqual(glob.web3Validator, null);
+            glob.ethersIoValidator = new ethers.Contract(glob.web3Validator.address, Validator.abi, glob.signer_owner);
+        }
+        catch (err) {
+            assert(false, 'Failed to instantiate Validator contract address. [Error: ' + err.toString() + ']');
+        }
+    });
+
     before("Preflight: Instantiate CommunityVote contract", async () => {
         try {
             glob.web3CommunityVote = await CommunityVote.deployed();
@@ -325,7 +336,7 @@ contract('Smart contract checks', function () {
 
     require('./scenarios/ClientFund')(glob);
     require('./scenarios/Hasher')(glob);
-    // require('./scenarios/Validator')(glob);
+    require('./scenarios/Validator')(glob);
     require('./scenarios/CommunityVote')(glob);
     require('./scenarios/Configuration')(glob);
     require('./scenarios/Exchange')(glob);
