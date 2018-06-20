f7756020d15c7425ede8cdcbf954a933f7de8c82
Jens Ivar Jordre <jensivar@hubii.com>
2018-06-20 20:12:21 +0200

    Update function and parameter names

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,25 +3,25 @@
  *
  * Copyright (C) 2017-2018 Hubii AS
  */
+
 const Migrations = artifacts.require("./Migrations.sol");
 
-var helpers = require('./helpers.js');
+const helpers = require('./helpers.js');
 
 // -----------------------------------------------------------------------------------------------------------------
 
-module.exports = function(deployer, network, accounts) {
-	var ownerAccount;
+module.exports = function (deployer, network, accounts) {
+    var ownerAccount;
 
-	if (helpers.isTestNetwork(network)) {
-		ownerAccount = accounts[0];
-	}
-	else {
-		ownerAccount = helpers.getOwnerAccountFromArgs();
-		ownerAccountPassword = helpers.getPasswordFromArgs();
-		helpers.unlockAddress(web3, ownerAccount, ownerAccountPassword, 1200); //20 minutes
-	}
+    if (helpers.isTestNetwork(network)) {
+        ownerAccount = accounts[0];
+    } else {
+        ownerAccount = helpers.getOwnerAccountFromArgs();
+        const ownerAccountPassword = helpers.getPasswordFromArgs();
+        helpers.unlockAddress(web3, ownerAccount, ownerAccountPassword, 1200); //20 minutes
+    }
 
-	deployer.deploy(Migrations, {
-		from : ownerAccount
-	});
+    deployer.deploy(Migrations, {
+        from: ownerAccount
+    });
 };
