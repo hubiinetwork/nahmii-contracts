9f55cf52bed49d6a6e4c530118172bdd900f59ad
Mauro Leggieri <mxmauro@mauroleggieri.com>
2018-06-28 13:58:10 -0300

    Added registration and who can change the address.

--- contracts/PartnerFund.sol
+++ contracts/PartnerFund.sol
@@ -13,12 +13,13 @@ import {SafeMathInt} from "./SafeMathInt.sol";
 import {Ownable} from "./Ownable.sol";
 import {ERC20} from "./ERC20.sol";
 import {SelfDestructible} from "./SelfDestructible.sol";
+import {Beneficiary} from "./Beneficiary.sol";
 
 /**
 @title ParnerFund
 @notice XXXX
 */
-contract ParnerFund is Ownable, SelfDestructible {
+contract ParnerFund is Ownable, Beneficiary, SelfDestructible {
     using SafeMathInt for int256;
 
     //
@@ -37,6 +38,10 @@ contract ParnerFund is Ownable, SelfDestructible {
     }
 
     struct PerWalletInfo {
+        bool isRegistered;
+        bool ownerCanChangeAddress;
+        bool canChangeAddress;
+
         uint256 fee;
         address wallet;
 
@@ -57,17 +62,18 @@ contract ParnerFund is Ownable, SelfDestructible {
     //
     // Variables
     // -----------------------------------------------------------------------------------------------------------------
-    mapping(bytes32 => PerWalletInfo) private walletInfoMap;
-    mapping(address => bytes32) private addressTagMap;
+    mapping(address => PerWalletInfo) private walletInfoMap;
+    mapping(address => address) private addressTagMap;
 
     //
     // Events
     // -----------------------------------------------------------------------------------------------------------------
-    event ChangedFeeEvent(bytes32 tag, uint256 fee);
-    event ChangedWalletEvent(bytes32 tag, address oldWallet, address newWallet);
-    event DepositEvent(bytes32 tag, int256 amount, address token); //token==0 for ethers
-    event StageEvent(bytes32 tag, address wallet, int256 amount, address token); //token==0 for ethers
-    event WithdrawEvent(bytes32 tag, address wallet, int256 amount, address token); //token==0 for ethers
+    event RegisterParnerEvent(address tag, uint256 fee);
+    event ChangedFeeEvent(address tag, uint256 fee);
+    event ChangedWalletEvent(address tag, address oldWallet, address newWallet);
+    event DepositEvent(address tag, int256 amount, address token); //token==0 for ethers
+    event StageEvent(address tag, address wallet, int256 amount, address token); //token==0 for ethers
+    event WithdrawEvent(address tag, address wallet, int256 amount, address token); //token==0 for ethers
 
     //
     // Constructor
@@ -78,7 +84,21 @@ contract ParnerFund is Ownable, SelfDestructible {
     //
     // Partner relationship functions
     // -----------------------------------------------------------------------------------------------------------------
-    function setPartnerFee(bytes32 tag, uint256 fee) public onlyOwner notNullTag(tag) {
+    function registerPartner(address tag, uint256 fee, bool canChangeAddress, bool ownerCanChangeAddress) public onlyOwner notNullTag(tag) {
+        require(!walletInfoMap[tag].isRegistered);
+        require(fee > 0);
+        require(canChangeAddress || ownerCanChangeAddress);
+
+        walletInfoMap[tag].isRegistered = true;
+        walletInfoMap[tag].ownerCanChangeAddress = ownerCanChangeAddress;
+        walletInfoMap[tag].canChangeAddress = canChangeAddress;
+        walletInfoMap[tag].fee = fee;
+
+        //raise event
+        emit RegisterParnerEvent(tag, fee);
+    }
+
+    function changePartnerFee(address tag, uint256 fee) public onlyOwner isRegisteredTag(tag) {
         require(fee > 0);
 
         walletInfoMap[tag].fee = fee;
@@ -87,14 +107,30 @@ contract ParnerFund is Ownable, SelfDestructible {
         emit ChangedFeeEvent(tag, fee);
     }
 
-    function getPartnerFee(bytes32 tag) public view notNullTag(tag) returns (uint256) {
+    function getPartnerFee(address tag) public view isRegisteredTag(tag) returns (uint256) {
         return walletInfoMap[tag].fee;
     }
 
-    function setPartnerAddress(bytes32 tag, address newWallet) public onlyOwner notNullTag(tag) {
-        //we allow wallet==0 to unlink
-        address oldWallet = walletInfoMap[tag].wallet;
+    function setPartnerAddress(address tag, address newWallet) public isRegisteredTag(tag) {
+        address oldWallet;
 
+        require(newWallet != owner);
+
+        oldWallet = walletInfoMap[tag].wallet;
+
+        //checks
+        if (isOwner()) {
+            require(walletInfoMap[tag].ownerCanChangeAddress);
+        }
+        else {
+            require(walletInfoMap[tag].canChangeAddress);
+
+            require(oldWallet != address(0) && msg.sender == oldWallet); //only the address owner can change it
+
+            require(newWallet != address(0)); //partners are not allowed to unlink the tag
+        }
+
+        //proceed with the change
         walletInfoMap[tag].wallet = newWallet;
 
         if (oldWallet != address(0))
@@ -106,7 +142,7 @@ contract ParnerFund is Ownable, SelfDestructible {
         emit ChangedWalletEvent(tag, oldWallet, newWallet);
     }
 
-    function getPartnerAddress(bytes32 tag) public view returns (address) {
+    function getPartnerAddress(address tag) public view returns (address) {
         return walletInfoMap[tag].wallet;
     }
 
@@ -117,7 +153,7 @@ contract ParnerFund is Ownable, SelfDestructible {
         revert();
     }
 
-    function receiveEthers(bytes32 tag) public notNullTag(tag) payable {
+    function receiveEthers(address tag) public isRegisteredTag(tag) payable {
         int256 amount = SafeMathInt.toNonZeroInt256(msg.value);
 
         walletInfoMap[tag].activeEtherBalance = walletInfoMap[tag].activeEtherBalance.add_nn(amount);
@@ -130,13 +166,13 @@ contract ParnerFund is Ownable, SelfDestructible {
     }
 
     //NOTE: 'wallet' must call ERC20.approve first
-    function receiveTokens(bytes32 tag, address sourceWallet, int256 amount, address token) public notNullTag(tag) {
+    function receiveTokens(address tag, int256 amount, address token) public isRegisteredTag(tag) {
         require(token != address(0));
         require(amount > 0);
 
         // Amount must be >0 so there's no problem with conversion to unsigned.
         ERC20 erc20 = ERC20(token);
-        require(erc20.transferFrom(sourceWallet, this, uint256(amount)));
+        require(erc20.transferFrom(msg.sender, this, uint256(amount)));
 
         walletInfoMap[tag].activeTokenBalance[token] = walletInfoMap[tag].activeTokenBalance[token].add_nn(amount);
 
@@ -147,7 +183,7 @@ contract ParnerFund is Ownable, SelfDestructible {
         emit DepositEvent(tag, amount, token);
     }
 
-    function deposit(bytes32 tag, uint index) public view notNullTag(tag) returns (int256 amount, address token, uint256 blockNumber) {
+    function deposit(address tag, uint index) public view isRegisteredTag(tag) returns (int256 amount, address token, uint256 blockNumber) {
         require(index < walletInfoMap[tag].depositsHistory.length);
 
         DepositHistory storage dh = walletInfoMap[tag].depositsHistory[index];
@@ -169,7 +205,7 @@ contract ParnerFund is Ownable, SelfDestructible {
         return deposit(partnerFromWallet(wallet), index);
     }
 
-    function depositCount(bytes32 tag) public view notNullTag(tag) returns (uint256) {
+    function depositCount(address tag) public view isRegisteredTag(tag) returns (uint256) {
         return walletInfoMap[tag].depositsHistory.length;
     }
 
@@ -181,7 +217,7 @@ contract ParnerFund is Ownable, SelfDestructible {
     // Staging functions
     // -----------------------------------------------------------------------------------------------------------------
     function stage(address token, int256 amount) public notOwner {
-        bytes32 tag = partnerFromWallet(msg.sender);
+        address tag = partnerFromWallet(msg.sender);
         require(amount.isPositiveInt256());
 
         if (token == address(0)) {
@@ -213,7 +249,7 @@ contract ParnerFund is Ownable, SelfDestructible {
     //
     // Balance functions
     // -----------------------------------------------------------------------------------------------------------------
-    function activeBalance(bytes32 tag, address token) public view notNullTag(tag) returns (int256) {
+    function activeBalance(address tag, address token) public view isRegisteredTag(tag) returns (int256) {
         if (token == address(0))
             return walletInfoMap[tag].activeEtherBalance;
         return walletInfoMap[tag].activeTokenBalance[token];
@@ -223,7 +259,7 @@ contract ParnerFund is Ownable, SelfDestructible {
         return activeBalance(partnerFromWallet(wallet), token);
     }
 
-    function stagedBalance(bytes32 tag, address token) public view notNullTag(tag) returns (int256) {
+    function stagedBalance(address tag, address token) public view isRegisteredTag(tag) returns (int256) {
         if (token == address(0))
             return walletInfoMap[tag].stagedEtherBalance;
         return walletInfoMap[tag].stagedTokenBalance[token];
@@ -237,7 +273,7 @@ contract ParnerFund is Ownable, SelfDestructible {
     // Withdrawal functions
     // -----------------------------------------------------------------------------------------------------------------
     function withdrawEther(int256 amount) public {
-        bytes32 tag = partnerFromWallet(msg.sender);
+        address tag = partnerFromWallet(msg.sender);
 
         if (amount > walletInfoMap[tag].stagedEtherBalance) {
             amount = walletInfoMap[tag].stagedEtherBalance;
@@ -254,7 +290,7 @@ contract ParnerFund is Ownable, SelfDestructible {
     }
 
     function withdrawTokens(address token, int256 amount) public {
-        bytes32 tag = partnerFromWallet(msg.sender);
+        address tag = partnerFromWallet(msg.sender);
         require(token != address(0));
 
         if (amount > walletInfoMap[tag].stagedTokenBalance[token]) {
@@ -275,17 +311,24 @@ contract ParnerFund is Ownable, SelfDestructible {
     //
     // Helpers
     // -----------------------------------------------------------------------------------------------------------------
-    function partnerFromWallet(address wallet) internal view returns (bytes32) {
-        bytes32 tag = addressTagMap[wallet];
+    function partnerFromWallet(address wallet) internal view returns (address) {
+        address tag = addressTagMap[wallet];
         require(tag != 0);
+        require(walletInfoMap[tag].isRegistered);
         return tag;
     }
 
     //
     // Modifiers
     // -----------------------------------------------------------------------------------------------------------------
-    modifier notNullTag(bytes32 tag) {
+    modifier notNullTag(address tag) {
         require(tag != 0);
+        _;
+    }
+
+    modifier isRegisteredTag(address tag) {
+        require(tag != 0);
+        require(walletInfoMap[tag].isRegistered);
         _;
     }
 }
