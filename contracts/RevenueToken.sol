63f2500112f5cc0ce1d58f1c5ac9536566c44792
Jens Ivar Jordre <jensivar@hubii.com>
2018-11-03 08:48:41 +0100

    Replace holdersEnum() by holdersByIndices()

--- contracts/RevenueToken.sol
+++ contracts/RevenueToken.sol
@@ -1,5 +1,5 @@
 /*
- * Hubii Nahmii
+/// Hubii Nahmii
  *
  * Compliant with the Hubii Nahmii specification v0.12.
  *
@@ -13,8 +13,8 @@ import {Ownable} from "./Ownable.sol";
 import {ERC20} from "./ERC20.sol";
 
 /**
- * @title RevenueToken
- * @dev Implementation of the EIP20 standard token (also known as ERC20 token) with addition of calculation of balance blocks
+@title RevenueToken
+@dev Implementation of the EIP20 standard token (also known as ERC20 token) with addition of calculation of balance blocks
  */
 contract RevenueToken is ERC20, Ownable {
     using SafeMathUintLib for uint256;
@@ -32,7 +32,6 @@ contract RevenueToken is ERC20, Ownable {
 
     address[] public holders;
     mapping(address => bool) holdersMap;
-    uint private holderEnumIndex;
 
     mapping(address => uint256) balances;
     mapping(address => uint256[]) balanceBlocks;
@@ -48,13 +47,15 @@ contract RevenueToken is ERC20, Ownable {
     // -----------------------------------------------------------------------------------------------------------------
     constructor() public ERC20() Ownable(msg.sender) {
         totalSupply = 0;
-        holderEnumIndex = 0;
     }
 
     //
     // Token settings
     // -----------------------------------------------------------------------------------------------------------------
-    function setTokenInformation(string newName, string newSymbol) public onlyDeployer {
+    function setTokenInformation(string newName, string newSymbol)
+    public
+    onlyDeployer
+    {
         require(bytes(newName).length > 0);
         require(bytes(newSymbol).length > 0 && bytes(newSymbol).length < 5);
 
@@ -68,12 +69,14 @@ contract RevenueToken is ERC20, Ownable {
     //
     // Functions
     // -----------------------------------------------------------------------------------------------------------------
-    /**
-     * @notice transfer token for a specified address
-     * @param _to The address to transfer to.
-     * @param value The amount to be transferred.
-     */
-    function transfer(address _to, uint256 value) public returns (bool success) {
+    /// @notice transfer token for a specified address
+    /// @param _to The address to transfer to.
+    /// @param value The amount to be transferred.
+    function transfer(address _to, uint256 value)
+    public
+    returns
+    (bool success)
+    {
         balances[msg.sender] = balances[msg.sender].sub(value);
         balances[_to] = balances[_to].add(value);
 
@@ -86,27 +89,30 @@ contract RevenueToken is ERC20, Ownable {
             holders.push(_to);
         }
 
-        //raise event
+        // Emit event
         emit Transfer(msg.sender, _to, value);
         return true;
     }
 
-    /**
-     * @notice Gets the balance of the specified address.
-     * @param account The address whose balance is to be queried.
-     * @return An uint256 representing the amount owned by the passed address.
-     */
-    function balanceOf(address account) public view returns (uint256 balance) {
+    /// @notice Gets the balance of the specified address.
+    /// @param account The address whose balance is to be queried.
+    /// @return An uint256 representing the amount owned by the passed address.
+    function balanceOf(address account)
+    public
+    view
+    returns (uint256 balance)
+    {
         return balances[account];
     }
 
-    /**
-     * @notice Transfer tokens from one address to another
-     * @param from address The address which you want to send tokens from
-     * @param to address The address which you want to transfer to
-     * @param value uint256 the amout of tokens to be transfered
-     */
-    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+    /// @notice Transfer tokens from one address to another
+    /// @param from address The address which you want to send tokens from
+    /// @param to address The address which you want to transfer to
+    /// @param value uint256 the amout of tokens to be transfered
+    function transferFrom(address from, address to, uint256 value)
+    public
+    returns (bool success)
+    {
         uint256 allowance = allowed[from][msg.sender];
 
         // Check is not needed because sub(allowance, value) will already throw if this condition is not met
@@ -126,17 +132,18 @@ contract RevenueToken is ERC20, Ownable {
             holders.push(to);
         }
 
-        //raise event
+        // Emit event
         emit Transfer(from, to, value);
         return true;
     }
 
-    /**
-     * @notice Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-     * @param spender The address which will spend the funds.
-     * @param value The amount of tokens to be spent.
-     */
-    function approve(address spender, uint256 value) public returns (bool success) {
+    /// @notice Approve the given address to spend the specified amount of tokens on behalf of msg.sender.
+    /// @param spender The address which will spend the funds.
+    /// @param value The amount of tokens to be spent.
+    function approve(address spender, uint256 value)
+    public
+    returns (bool success)
+    {
         // To change the approve amount you first have to reduce the addresses'
         //  allowance to zero by calling `approve(spender, 0)` if it is not
         //  already 0 to mitigate the race condition described here:
@@ -145,50 +152,54 @@ contract RevenueToken is ERC20, Ownable {
 
         allowed[msg.sender][spender] = value;
 
-        //raise event
+        // Emit event
         emit Approval(msg.sender, spender, value);
         return true;
     }
 
-    /**
-     * @notice Function to check the amount of tokens than an owner allowed to a spender.
-     * @param account address The address which owns the funds.
-     * @param spender address The address which will spend the funds.
-     * @return A uint256 specifing the amount of tokens still avaible for the spender.
-     */
-    function allowance(address account, address spender) public view returns (uint256) {
+    /// @notice Function to check the amount of tokens than an owner allowed to a spender.
+    /// @param account address The address which owns the funds.
+    /// @param spender address The address which will spend the funds.
+    /// @return A uint256 specifing the amount of tokens still avaible for the spender.
+    function allowance(address account, address spender)
+    public
+    view
+    returns (uint256)
+    {
         return allowed[account][spender];
     }
 
-    /**
-     * @notice Increase the amount of tokens that an owner allowed to a spender.
-     *
-     * approve should be called when allowed[_spender] == 0. To increment
-     * allowed value is better to use this function to avoid 2 calls (and wait until
-     * the first transaction is mined)
-     * From MonolithDAO Token.sol
-     * @param _spender The address which will spend the funds.
-     * @param _addedValue The amount of tokens to increase the allowance by.
-     */
-    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    /// @notice Increase the amount of tokens that an owner allowed to a spender.
+    ///
+    /// approve() should be called when allowed[_spender] == 0. To increment
+    /// allowed value is better to use this function to avoid 2 calls (and wait until
+    /// the first transaction is mined)
+    /// From MonolithDAO Token.sol
+    /// @param _spender The address which will spend the funds.
+    /// @param _addedValue The amount of tokens to increase the allowance by.
+    function increaseApproval(address _spender, uint _addedValue)
+    public
+    returns (bool)
+    {
         allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
 
-        //raise event
+        // Emit event
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    /**
-     * @notice Decrease the amount of tokens that an owner allowed to a spender.
-     *
-     * approve should be called when allowed[_spender] == 0. To decrement
-     * allowed value is better to use this function to avoid 2 calls (and wait until
-     * the first transaction is mined)
-     * From MonolithDAO Token.sol
-     * @param _spender The address which will spend the funds.
-     * @param _subtractedValue The amount of tokens to decrease the allowance by.
-     */
-    function decreaseApproval(address _spender, uint256 _subtractedValue) public returns (bool success) {
+    /// @notice Decrease the amount of tokens that an owner allowed to a spender.
+    ///
+    /// approve() should be called when allowed[_spender] == 0. To decrement
+    /// allowed value is better to use this function to avoid 2 calls (and wait until
+    /// the first transaction is mined)
+    /// From MonolithDAO Token.sol
+    /// @param _spender The address which will spend the funds.
+    /// @param _subtractedValue The amount of tokens to decrease the allowance by.
+    function decreaseApproval(address _spender, uint256 _subtractedValue)
+    public
+    returns (bool success)
+    {
         uint256 oldValue = allowed[msg.sender][_spender];
 
         if (_subtractedValue > oldValue) {
@@ -197,18 +208,20 @@ contract RevenueToken is ERC20, Ownable {
             allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
 
-        //raise event
+        // Emit event
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    /**
-     * @notice Function to mint tokens
-     * @param _to The address that will receive the minted tokens.
-     * @param _amount The amount of tokens to mint.
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function mint(address _to, uint256 _amount) onlyDeployer public returns (bool) {
+    /// @notice Function to mint tokens
+    /// @param _to The address that will receive the minted tokens.
+    /// @param _amount The amount of tokens to mint.
+    /// @return A boolean that indicates if the operation was successful.
+    function mint(address _to, uint256 _amount)
+    onlyDeployer
+    public
+    returns (bool)
+    {
         totalSupply = totalSupply.add(_amount);
         balances[_to] = balances[_to].add(_amount);
 
@@ -224,15 +237,17 @@ contract RevenueToken is ERC20, Ownable {
         return true;
     }
 
-    /**
-    * @notice Calculate the amount of balance blocks, i.e. the area under the curve (AUC) of balance as function of block number
-    * @dev The AUC is used as weight for the share of revenue that a token holder may claim
-    * @param wallet The wallet address for which calculation is done
-    * @param startBlock The start block number considered
-    * @param endBlock The end block number considered
-    * @return The calculated AUC
-    */
-    function balanceBlocksIn(address wallet, uint256 startBlock, uint256 endBlock) public view returns (uint256) {
+    /// @notice Calculate the amount of balance blocks, i.e. the area under the curve (AUC) of balance as function of block number
+    /// @dev The AUC is used as weight for the share of revenue that a token holder may claim
+    /// @param wallet The wallet address for which calculation is done
+    /// @param startBlock The start block number considered
+    /// @param endBlock The end block number considered
+    /// @return The calculated AUC
+    function balanceBlocksIn(address wallet, uint256 startBlock, uint256 endBlock)
+    public
+    view
+    returns (uint256)
+    {
         uint256 idx;
         uint256 low;
         uint256 res;
@@ -283,35 +298,39 @@ contract RevenueToken is ERC20, Ownable {
         return res;
     }
 
-    //HOW TO USE:
-    //1. Owner checks current block number and calls startHoldersEnum
-    //2. If current block number != block number stored in step 1, go to step 1
-    //3. Owner calls holdersEnum and saves returned addresses until address[index] == 0
-    //4. If address[0] == 0, go to step 2
-    //5. Done
-
-    //A cheaper alternative is to monitor all Transfer events and store them in a database.
-    function startHoldersEnum() onlyDeployer public {
-        holderEnumIndex = 0;
+    /// @notice Get the count of holders
+    /// @return The number of holders
+    function holdersCount()
+    public
+    view
+    returns (uint256)
+    {
+        return holders.length;
     }
 
-    function holdersEnum() onlyDeployer public returns (address[]) {
-        address[] memory _holders = new address[](1024);
-        uint256 counter = 0;
+    /// @notice Get the subset of holders with positive balance in the given 0 based index range
+    /// @param low The lower inclusive index
+    /// @param up The upper inclusive index
+    /// @return The subset of registered holders
+    function holdersByIndices(uint256 low, uint256 up)
+    public
+    view
+    returns (address[])
+    {
+        require(low <= up);
 
-        while (counter < _holders.length && holderEnumIndex < holders.length) {
-            if (balances[holders[holderEnumIndex]] > 0) {
-                _holders[counter] = holders[holderEnumIndex];
-                counter++;
-            }
-            holderEnumIndex++;
-        }
+        low = low < 0 ? 0 : low;
+        up = up > holders.length - 1 ? holders.length - 1 : up;
+        address[] memory _holders = new address[](up - low + 1);
+        for (uint256 i = low; i <= up; i++)
+            if (0 < balances[holders[i]])
+                _holders[i - low] = holders[i];
 
         return _holders;
     }
 
     //
-    // Internal functions
+    // Private functions
     // -----------------------------------------------------------------------------------------------------------------
     function addBalanceBlocks(address _to) private {
         uint256 len;
