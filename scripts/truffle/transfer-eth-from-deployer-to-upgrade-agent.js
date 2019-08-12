8a1ac07f829265adc4cb5525ed76809d06c43fbe
Jens Ivar Jordre <jensivar@hubii.com>
2019-08-12 11:18:50 +0200

    Add truffle script to transfer ETH

---
+++ scripts/truffle/transfer-eth-from-deployer-to-upgrade-agent.js
@@ -0,0 +1,38 @@
+cd node_modules/nahmii-contract-abstractions-ropsten
+
+cat > script.js << EOF
+module.exports = async (callback) => {
+
+    const from = process.env.FROM;
+    const password = process.env.PASSWORD;
+    const to = process.env.TO;
+    const value = process.env.VALUE;
+    const gasPrice = process.env.GAS_PRICE;
+
+    try {
+        await web3.eth.personal.unlockAccount(from, password, 600);
+
+        const nonce = await web3.eth.getTransactionCount(from);
+
+        const tx = {
+            from,
+            to,
+            value: web3.utils.toWei(value, 'ether'),
+            gasPrice,
+            gas: '21000',
+            nonce
+        };
+
+        const signedTx = await web3.eth.personal.signTransaction(tx, password);
+
+        const sentTx = await web3.eth.sendSignedTransaction(signedTx.raw);
+        console.log(sentTx);
+
+        callback()
+    } catch (e) {
+        callback(e);
+    } finally {
+        web3.eth.personal.lockAccount(from);
+    }
+};
+EOF
