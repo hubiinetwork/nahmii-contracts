cb60cff4ce6440a148e3b1a8ec0a0e1bdf4ffb15
Jens Ivar Jordre <jensivar@hubii.com>
2019-09-17 11:40:27 +0200

    Clean up

--- truffle.js
+++ truffle.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 // For read-only interaction
 const mnemonic = 'paddle envelope cage erupt lake unaware genre captain thunder spread hollow hybrid notice kangaroo wasp';
@@ -6,65 +6,59 @@ const mnemonic = 'paddle envelope cage erupt lake unaware genre captain thunder 
 module.exports = {
     networks: {
         development: {
-            host: "localhost",
+            host: 'localhost',
             port: 8545,
-            network_id: "*",
+            network_id: '*',
             gas: 8000000
         },
         develop: {
-            host: "localhost",
+            host: 'localhost',
             port: 9545,
-            network_id: "*",
+            network_id: '*',
             gas: 8000000
         },
         ganache: {
-            host: "localhost",
+            host: 'localhost',
             port: 7545,
-            network_id: "*",
+            network_id: '*',
             gas: 8000000
         },
-        "ganache-cli": {
-            host: "localhost",
+        'ganache-cli': {
+            host: 'localhost',
             port: 8545,
-            network_id: "*",
+            network_id: '*',
             gas: 8000000
         },
         ropsten: {
-            host: "geth-ropsten.ethereum",
+            host: 'geth-ropsten.ethereum',
             port: 80,
-            network_id: "*",
+            network_id: '3',
             gas: 8000000
         },
-        "ropsten-infura": {
+        'ropsten-infura': {
             provider: function () {
-                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/36deff216fd744b9bfba9f884df9fdc3");
+                return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/36deff216fd744b9bfba9f884df9fdc3');
             },
-            network_id: "*",
-            gas: 8000000
-        },
-        rinkeby: {
-            host: "geth-rinkeby.ethereum",
-            port: 80,
-            network_id: "*",
+            network_id: '3',
             gas: 8000000
         },
         mainnet: {
-            host: "geth-homestead.ethereum",
+            host: 'geth-homestead.ethereum',
             port: 80,
-            network_id: "1",
+            network_id: '1',
             gas: 8000000
         },
-        "mainnet-infura": {
+        'mainnet-infura': {
             provider: function () {
-                return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/36deff216fd744b9bfba9f884df9fdc3");
+                return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/36deff216fd744b9bfba9f884df9fdc3');
             },
-            network_id: "*",
+            network_id: '1',
             gas: 8000000
         },
-        "mainnet-hubii": {
-            host: "ethereum.hubii.com",
+        'mainnet-hubii': {
+            host: 'ethereum.hubii.com',
             port: 8545,
-            network_id: "1",
+            network_id: '1',
             gas: 8000000
         }
     },
