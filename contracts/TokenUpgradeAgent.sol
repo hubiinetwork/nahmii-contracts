f010e2aa78ee0fe3911496d31398aa3b058617d0
Jens Ivar Jordre <jensivar@hubii.com>
2019-11-04 18:59:18 +0100

    Add abstract upgrade agent contract

---
+++ contracts/TokenUpgradeAgent.sol
@@ -0,0 +1,42 @@
+/*
+ * Hubii Nahmii
+ *
+ * Compliant with the Hubii Nahmii specification v0.12.
+ *
+ * Copyright (C) 2017-2018 Hubii AS
+ */
+
+pragma solidity >=0.4.25 <0.6.0;
+
+/**
+ * @title TokenUpgradeAgent
+ * @notice A contract that transfers token to a new contract
+ * @dev A descendant of this contract should under most circumstances be able
+ * to denounce itself as minter of the new token after a successful upgrade
+ * of the total supply of the old token.
+ */
+contract TokenUpgradeAgent {
+
+    address public origin;
+
+    constructor(address _origin)
+    public
+    {
+        origin = _origin;
+    }
+
+    //
+    // Functions
+    // -----------------------------------------------------------------------------------------------------------------
+    function upgradeFrom(address from, uint256 value)
+    public
+    returns (bool);
+
+    //
+    // Modifiers
+    // -----------------------------------------------------------------------------------------------------------------
+    modifier onlyOrigin() {
+        require(msg.sender == origin);
+        _;
+    }
+}
